439c642efbf4d6cafd94255ba18256bc683dbb70
Alexander Green <alexander.green@logius.nl>
2025-07-02 16:38:17 +0200

    versienummer en -datum

--- js/config.js
+++ js/config.js
@@ -19,11 +19,11 @@ var respecConfig = {
         "name" : "Peter Haasnoot"
       } ],
   github: "https://github.com/Logius-standaarden/OIN-Stelsel",
-  previousPublishVersion: "2.0.3",
-  previousPublishDate: "2020-03-31",
+  previousPublishVersion: "2.1.0",
+  previousPublishDate: "2024-01-19",
   pubDomain: "dk",
-  publishDate: "2024-01-19",
-  publishVersion: "2.1.0",
+  publishDate: "2025-06-10",
+  publishVersion: "2.2.0",
   shortName: "oin",
   specStatus: "WV",
   specType: "HR"
